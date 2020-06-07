b3a550b47c05fc5e8ed7468aaf3245afd6ac2640
Manue Ortiz Hernández <manuoh09@gmail.com>
2020-06-06 21:30:06 -0500

    Guardar archivo en apuntador

--- main.c
+++ main.c
@@ -2,7 +2,29 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/types.h>
 
-int main(){
-	printf("Hola");
+
+int main(int argc, const char* argv[]){
+    int f_map = open("./Files/alice.txt", O_RDONLY, S_IRUSR | S_IWUSR);
+    struct stat sb;
+    if (fstat(f_map, &sb) == -1)
+        perror("Error en size ");
+    printf("El tamaño del archivo es: %lld\n", sb.st_size);
+    //Guardamos en un apuntador el archivo
+    char *file_in_memory = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, f_map, 0);
+
+    /* IMPRIMIR TODO EL FILE EN LA CONSOLA:
+    for(int i = 0; i<sb.st_size; i++){
+        printf("%c", file_in_memory[i]);
+    }
+    printf("\n");*/
+    munmap(file_in_memory, sb.st_size);
+    close(f_map);
+
+    return 0;
 }
---
+++ map.h
@@ -0,0 +1,15 @@
+typedef struct Map{
+    Key key;
+    Value value;
+} Map;
+
+typedef struct Key{
+    int f;
+    int t;
+} Key;
+
+typedef struct Value{
+    int algo;
+    int algoMas;
+} Value;
+
