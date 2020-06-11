634cd38989f1e69084c185afdda1516f03495e8d
Manue Ortiz Hernández <manuoh09@gmail.com>
2020-06-11 18:58:58 -0500

    Se agregó el Mapa conteniendo todas las palabras

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int map_code(char *key){
     for(i = 0; i < strlen(key); i++){
         value = value * 10 + key[i];
     }
-    return value % 2000;
+    return value % sb.st_size;
 }
 
 Mapa *crear_mapa(void){
@@ -117,6 +117,7 @@ int main(int argc, const char* argv[]){
     Mapa *mapa3 = NULL;
     Mapa *palabrasReducidas = NULL;
     
+    
     Mapa *palabrasOrdenadas = filtrar_palabras();
     int f_map = open("./Files/home.txt", O_RDONLY, S_IRUSR | S_IWUSR);
     if (fstat(f_map, &sb) == -1)
@@ -124,13 +125,12 @@ int main(int argc, const char* argv[]){
     printf("El tamaño del archivo es: %lld\n", sb.st_size);
     
     //Mapa *palabras = mapper(0, sb.st_size);
-    
     //Guardamos en un apuntador el archivo
     file_in_memory = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, f_map, 0);
     pid_t p1, p2, p3;
     int status;
     
-    if((p1=fork())== 0){
+    /*if((p1=fork())== 0){
        mapa1 = mapper(0, sb.st_size/3);
        palabrasReducidas = reduce(palabrasOrdenadas, mapa1);
        sleep(1);
@@ -152,10 +152,10 @@ int main(int argc, const char* argv[]){
        waitpid(p2, &status, 0);
        waitpid(p3, &status, 0);
        
-    }
+    }*/
     printf("Salí\n");
     //TODO ESTO DEBE PASAR DE FORMA SIMULTÁNEA
-    /*Mapa *palabras = mapper(0, sb.st_size);
+    Mapa *palabras = mapper(0, sb.st_size); //Esta línea, si la pones arriba de los procesos, explota el programa. I don't know why
     mapa1 = mapper(0, sb.st_size/3);
     mapa2 = mapper(sb.st_size/3, sb.st_size/3+sb.st_size/3);
     mapa3 = mapper(sb.st_size/3+sb.st_size/3, sb.st_size);
@@ -163,7 +163,7 @@ int main(int argc, const char* argv[]){
     palabrasReducidas = reduce(palabrasOrdenadas, mapa1);
     palabrasReducidas = reduce(palabrasReducidas, mapa2);
     palabrasReducidas = reduce(palabrasReducidas, mapa3);
-    imprime(palabrasReducidas, palabras);*/
+    imprime(palabrasReducidas, palabras);
 
     printf("\n");
     munmap(file_in_memory, sb.st_size);
