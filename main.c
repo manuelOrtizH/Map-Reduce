4aba3729bdef5e34e65874f8906bb0fdcb9314a7
Manue Ortiz Hernández <manuoh09@gmail.com>
2020-06-11 18:49:38 -0500

    Métodos de imprimir, leer y buscar.

--- main.c
+++ main.c
@@ -60,18 +60,36 @@ Mapa* mapper(int from, int to){
     return mapa;
 }
 
-void busca(Mapa *mapa){
-    
+void lee(Mapa *mapa){
     for(int i = 0; i<mapa->counterWords; i++){
         printf("The key of this map is: %s   .The value of the string is: %d\n", mapa->entradas[i]->key, mapa->entradas[i]->value);
     }
-    /*char* str = malloc(30);
-    str = "Home";
+}
+
+void busca(Mapa *mapa){
+    char* str = malloc(30);
+    str = "goodnight";
     int mapCode = map_code(str);
     printf("MapCode of %s is %d\n", str, mapCode);
     printf("The num of appearances of %s is %d", mapa->entradas[mapCode]->key, mapa->entradas[mapCode]->value);
-    */
 }
+
+void imprime(Mapa *mapa, Mapa *palabras){
+    int code;
+    int prev = 0;
+
+    for(int i = 0; i<palabras->counterWords; i++){
+        code = map_code(palabras->entradas[i]->key);
+        if(prev == code){//Ya se imprimió
+            continue;
+        }
+        if(code>0){
+            printf("%s: %d\n", mapa->entradas[code]->key, mapa->entradas[code]->value);
+        }
+        prev = code;
+    }
+}
+
 
 Mapa* reduce(Mapa *palabrasReducidas, Mapa *mapa){
     int mapCode;
@@ -98,54 +116,56 @@ int main(int argc, const char* argv[]){
     Mapa *mapa2 = NULL;
     Mapa *mapa3 = NULL;
     Mapa *palabrasReducidas = NULL;
+    
     Mapa *palabrasOrdenadas = filtrar_palabras();
     int f_map = open("./Files/home.txt", O_RDONLY, S_IRUSR | S_IWUSR);
     if (fstat(f_map, &sb) == -1)
         perror("Error en size ");
     printf("El tamaño del archivo es: %lld\n", sb.st_size);
     
+    //Mapa *palabras = mapper(0, sb.st_size);
+    
     //Guardamos en un apuntador el archivo
     file_in_memory = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, f_map, 0);
     pid_t p1, p2, p3;
     int status;
+    
     if((p1=fork())== 0){
        mapa1 = mapper(0, sb.st_size/3);
-       busca(mapa1);
+       palabrasReducidas = reduce(palabrasOrdenadas, mapa1);
        sleep(1);
        exit(0);
     }else if((p2=fork())==0){
        mapa2 = mapper(sb.st_size/3, sb.st_size/3+sb.st_size/3);
-       busca(mapa2);
+       sleep(1);
+       palabrasReducidas = reduce(palabrasReducidas, mapa2);
        sleep(1);
        exit(0);
     }else if((p3=fork())==0){
        mapa3 = mapper(sb.st_size/3+sb.st_size/3, sb.st_size);
+       sleep(2);
+       palabrasReducidas = reduce(palabrasReducidas, mapa3);
        sleep(1);
        exit(0);
     }else{
        waitpid(p1, &status, 0);
        waitpid(p2, &status, 0);
        waitpid(p3, &status, 0);
+       
     }
+    printf("Salí\n");
     //TODO ESTO DEBE PASAR DE FORMA SIMULTÁNEA
-    /*mapa1 = mapper(0, sb.st_size/3);
+    /*Mapa *palabras = mapper(0, sb.st_size);
+    mapa1 = mapper(0, sb.st_size/3);
     mapa2 = mapper(sb.st_size/3, sb.st_size/3+sb.st_size/3);
-    mapa3 = mapper(sb.st_size/3+sb.st_size/3, sb.st_size);*/
+    mapa3 = mapper(sb.st_size/3+sb.st_size/3, sb.st_size);
     
-    /*palabrasReducidas = reduce(palabrasOrdenadas, mapa1);
+    palabrasReducidas = reduce(palabrasOrdenadas, mapa1);
     palabrasReducidas = reduce(palabrasReducidas, mapa2);
     palabrasReducidas = reduce(palabrasReducidas, mapa3);
-    //busca(mapa1);
-    busca(palabrasReducidas);*/
-    
-    /*
-    */
-    
-    
-    
-    //IMPRIMIR TODO EL FILE EN LA CONSOLA:
+    imprime(palabrasReducidas, palabras);*/
 
-    printf("\n");//*/
+    printf("\n");
     munmap(file_in_memory, sb.st_size);
     close(f_map);
 
